a721ef8b553108c3c7599460f57cda4080002481
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-08 07:44:06 +0530

    Change to assert method

--- StudentRepo.java
+++ StudentRepo.java
@@ -21,21 +21,34 @@ class StudentRepo{
         //Name validation
         System.out.print("Enter name Student 1: ");
         name = scanner.nextLine();
-        if (name.strip() == ""){
-            System.out.printf(error, "Name");
-        }else{
-            System.out.print("Enter your Age: ");
-            age = scanner.nextInt();
-            if (age<10 && 18 < age ){
-                System.out.printf(error, "Age");
-            }else{
-                System.out.print("Enter subject 1: ");
-                sub1 = scanner.nextLine();
-                if(!sub1.startsWith("SE-") && sub1.strip() == ""){
-                    System.out.printf(error,"Subject");
-                }
-            }
-        }
+        assert name.strip() != "" : "\033[1;31mInvalid Name\033[0m";
+
+        //Age validation
+        System.out.print("Enter your Age: ");
+        age = scanner.nextInt();
+        assert age > 10 && 18 > age :"\033[1;31mInvalid Age\033[0m";
+    
+        //subject validation
+        System.out.print("Enter subject 1: ");
+        scanner.nextLine();
+        sub1 = scanner.nextLine();
+        assert sub1.startsWith("SE-") == true && sub1.strip() != "" : "\033[1;31mInvalid Subject\033[0m";
+        
+        System.out.print("Enter subject 2: ");
+        scanner.nextLine();
+        sub1 = scanner.nextLine();
+        assert sub1.startsWith("SE-") == true && sub1.strip() != "" : "\033[1;31mInvalid Subject\033[0m"; 
+
+        System.out.print("Enter subject 3: ");
+        scanner.nextLine();
+        sub1 = scanner.nextLine();
+        assert sub1.startsWith("SE-") == true && sub1.strip() != "" : "\033[1;31mInvalid Subject\033[0m"; 
+
+        //marks validation
+        assert 
+
+        
+
 
 
     }
